99f50e438d17216fae132314b7bc52922623d36b
Hugo Feuillâtre <hugo.feuillatre@gmail.com>
2016-06-03 00:07:20 +0200

    added a main for DictionaryGenerator, print the dictionary in the appropriate format, modified regex (still working on it), remove lang field from DictionaryGenerator

--- src/jspellfh/DictionaryGenerator.java
+++ src/jspellfh/DictionaryGenerator.java
@@ -4,45 +4,45 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DictionaryGenerator {
-	private String lang;
 	private HashMap<String, Float> wordList;
-	private int totalWords; 
-	
-	public DictionaryGenerator(String lang) {
-		this.lang = lang;
+
+	public DictionaryGenerator() {
 		wordList = new HashMap<String, Float>();
-		totalWords = 0;
 	}
 	
 	public void generateDictionary(String folder) {
-		Pattern p = Pattern.compile("[a-zA-Z'\\-.]+[a-zA-Z]|[a-zA-Z'-]+\\s*");
 		try {
 			Files.walk(Paths.get(folder)).forEach(filePath -> {
 				if(Files.isRegularFile(filePath)) {
 					Scanner sc = null;
 					try {
 						sc = new Scanner(filePath);
+						//sc.useDelimiter(Pattern.compile(""));
 					} catch (IOException eIOE) {
 						System.out.println("Error with file : " + filePath + " " + eIOE);
 					}
 					
 					while(sc.hasNext()) {
-						String word = sc.next().toLowerCase();
+						// La regex remplace les apostrophes... A corriger
+						String word = sc.next().toLowerCase().replaceAll("\\p{Ps}|\\p{Pe}|\\p{Pi}|\\p{Pf}|\\p{Pc}|\\p{Po}", "");
+						Pattern p = Pattern.compile("[a-zA-ZÀ-ÿ'\\-.]+[a-zA-ZÀ-ÿ]|[a-zA-ZÀ-ÿ'-]+\\s*");
 						Matcher m = p.matcher(word);
 
 						if(m.matches()){
 							if(wordList.containsKey(word)) {
 								wordList.replace(word, wordList.get(word) + 1);
 							} else {
-								wordList.put(word, (float)1);
+								wordList.put(word, (float) 1);
 							}
-							totalWords++;
-						}
+						}/* else {
+							System.out.println("Not matched : " + word);
+						}*/
 					}
 				}
 			});
@@ -53,11 +53,25 @@ public class DictionaryGenerator {
 		}
 	}
 
-	public String getLang() {
-		return lang;
-	}
-
 	public HashMap<String, Float> getWordList() {
 		return wordList;
+	}
+
+	public int getTotalWords() {
+		return wordList.size();
+	}
+
+	public void printDictionary() {
+		for(Map.Entry<String, Float> entry : wordList.entrySet()) {
+			String key = entry.getKey();
+			Float value = entry.getValue();
+			System.out.println(key + '\t' + value);
+		}
+	}
+
+	public static void main(String[] args) {
+		DictionaryGenerator dg = new DictionaryGenerator();
+		dg.generateDictionary(args[0]);
+		dg.printDictionary();
 	}
 }
--- src/jspellfh/Main.java
+++ src/jspellfh/Main.java
@@ -2,9 +2,10 @@ package jspellfh;
 
 public class Main {
 	public static void main(String[] args) {
-		DictionaryGenerator dg = new DictionaryGenerator("fr");
+		DictionaryGenerator dg = new DictionaryGenerator();
 		dg.generateDictionary(args[0]);
 		
 		System.out.println(dg.getWordList());
+		System.out.println(dg.getTotalWords());
 	}
 }
