f9b115f5c030c3cdba862a2d091b4b364a51d5d1
Hugo Feuillâtre <hugo.feuillatre@gmail.com>
2016-06-07 21:59:55 +0200

    modified Dictionary hashmap<string, integer> to <string, float>, put back missing code (?), begun work on SpellAnnotator : now read dictionaries to create one single hashmap from all of them, and read system.in but do nothing

--- src/jspellfh/DictionaryGenerator.java
+++ src/jspellfh/DictionaryGenerator.java
@@ -11,44 +11,47 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DictionaryGenerator {
-	private HashMap<String, Integer> wordList;
+	private HashMap<String, Float> wordList;
 
 	public DictionaryGenerator() {
 		wordList = new HashMap<>();
 	}
 
-    private void readFile(Path filePath) {
-        if(Files.isRegularFile(filePath)) {
-            Scanner sc = null;
-            try {
-                sc = new Scanner(filePath);
-                //sc.useDelimiter(Pattern.compile(""));
-            } catch (IOException eIOE) {
-                System.out.println("Error with file : " + filePath + " " + eIOE);
-            }
+	private void readFile(Path filePath) {
+		if(Files.isRegularFile(filePath)) {
+			Scanner sc = null;
+			try {
+				sc = new Scanner(filePath);
+				//sc.useDelimiter(Pattern.compile(""));
+			} catch (IOException eIOE) {
+				System.out.println("Error with file : " + filePath + " " + eIOE);
+			}
 
-            while(sc.hasNext()) {
-                // La regex remplace les apostrophes... A corriger
-                String word = sc.next().toLowerCase().replaceAll("\\p{Ps}|\\p{Pe}|\\p{Pi}|\\p{Pf}|\\p{Pc}|\\p{Po}", "");
-                Pattern p = Pattern.compile("[a-zA-ZÀ-ÿ'\\-.]+[a-zA-ZÀ-ÿ]|[a-zA-ZÀ-ÿ'-]+\\s*");
-                Matcher m = p.matcher(word);
+			while(sc.hasNext()) {
+				// La regex remplace les apostrophes... A corriger
+				String word = sc.next().toLowerCase().replaceAll("\\p{Ps}|\\p{Pe}|\\p{Pi}|\\p{Pf}|\\p{Pc}|\\p{Po}", "");
+				Pattern p = Pattern.compile("[a-zA-ZÀ-ÿ'\\-.]+[a-zA-ZÀ-ÿ]|[a-zA-ZÀ-ÿ'-]+\\s*");
+				Matcher m = p.matcher(word);
 
-                if(m.matches()){
-                    if(wordList.containsKey(word)) {
-                        wordList.replace(word, wordList.get(word) + 1);
-                    } else {
-                        wordList.put(word, 1);
-                    }
-                }/* else {
-							System.out.println("Not matched : " + word);
-						}*/
-            }
-        }
-    }
-	
+				if(m.matches()){
+					if(wordList.containsKey(word)) {
+						wordList.replace(word, wordList.get(word) + 1);
+					} else {
+						wordList.put(word, (float) 1);
+					}
+				}/* else {
+					System.out.println("Not matched : " + word);
+				}*/
+			}
+		}
+	}
+
 	public void generateDictionary(String folder) {
 		try {
 			Files.walk(Paths.get(folder)).forEach(filePath -> readFile(filePath));
+			/* Update word frequency */
+			for(Map.Entry<String, Float> entry : wordList.entrySet())
+				entry.setValue(entry.getValue() / wordList.size());
 		} catch (IOException eIOE) {
 			System.out.println("It was impossible to parse files in directory. " + eIOE);
 		} catch (SecurityException eSE) {
@@ -56,7 +59,7 @@ public class DictionaryGenerator {
 		}
 	}
 
-	public HashMap<String, Integer> getWordList() {
+	public HashMap<String, Float> getWordList() {
 		return wordList;
 	}
 
@@ -65,9 +68,9 @@ public class DictionaryGenerator {
 	}
 
 	public void printDictionary() {
-		for(Map.Entry<String, Integer> entry : wordList.entrySet()) {
+		for(Map.Entry<String, Float> entry : wordList.entrySet()) {
 			String key = entry.getKey();
-            Integer value = entry.getValue();
+			Float value = entry.getValue();
 			System.out.println(key + '\t' + value);
 		}
 	}
